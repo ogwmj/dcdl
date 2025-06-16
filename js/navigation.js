0c7973d704619c191eda931c3caf64ee0b5fda12
ogwmj <dclegiondark@proton.me>
2025-06-16 07:22:02 -0400

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -1,24 +1,29 @@
-// JavaScript to handle active link highlighting and mobile menu toggle
-  document.addEventListener('DOMContentLoaded', function () {
-    const currentPath = window.location.pathname.split("/").pop();
-    const navLinks = document.querySelectorAll('.nav-link');
-    navLinks.forEach(link => {
-      if (link.getAttribute('href') === currentPath) {
-        link.classList.add('active');
-      }
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  const currentPath = window.location.pathname.split("/").pop();
+  const navLinks = document.querySelectorAll('.nav-link');
 
-    const mobileMenuButton = document.getElementById('mobile-menu-button');
-    const mobileMenu = document.getElementById('mobile-menu');
-    const menuIcons = mobileMenuButton.querySelectorAll('svg');
-
-    if (mobileMenuButton && mobileMenu) {
-      mobileMenuButton.addEventListener('click', function () {
-        const expanded = this.getAttribute('aria-expanded') === 'true' || false;
-        this.setAttribute('aria-expanded', !expanded);
-        mobileMenu.classList.toggle('hidden');
-        menuIcons[0].classList.toggle('hidden'); // Toggle menu icon
-        menuIcons[1].classList.toggle('hidden'); // Toggle x icon
-      });
+  navLinks.forEach(link => {
+    const linkPath = link.getAttribute('href');
+    if (linkPath === currentPath || (currentPath === '' && linkPath === 'index.html')) {
+      link.classList.add('active');
     }
   });
+
+  const mobileMenuButton = document.getElementById('mobile-menu-button');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  if (mobileMenuButton && mobileMenu) {
+    const menuIcons = mobileMenuButton.querySelectorAll('svg');
+    
+    mobileMenuButton.addEventListener('click', function () {
+      const isExpanded = this.getAttribute('aria-expanded') === 'true' || false;
+      this.setAttribute('aria-expanded', !isExpanded);
+      mobileMenu.classList.toggle('hidden');
+      
+      if (menuIcons && menuIcons.length === 2) {
+          menuIcons[0].classList.toggle('hidden');
+          menuIcons[1].classList.toggle('hidden');
+      }
+    });
+  }
+});
