0575f8c15fe9127c761f886ba6c8571c62653146
ogwmj <dclegiondark@proton.me>
2025-06-16 14:59:06 -0400

    Responsive Table

--- js/teams/ui.js
+++ js/teams/ui.js
@@ -620,16 +620,10 @@ function renderRosterTable() {
 
     if (typeof $ !== 'undefined' && $.fn.DataTable) {
         rosterDataTable = new $('#roster-table').DataTable({
-            responsive: true,
+            scrollX: true,
             order: [[4, 'desc']],
             columnDefs: [
-                { responsivePriority: 1, targets: 0 },
-                { responsivePriority: 2, targets: 6, orderable: false },
-                { responsivePriority: 3, targets: 1 },
-                { responsivePriority: 4, targets: 2 },
-                { responsivePriority: 5, targets: 3 },
-                { responsivePriority: 6, targets: 4 },
-                { responsivePriority: 7, targets: 5 }
+                { targets: 6, orderable: false } 
             ]
         });
     }
